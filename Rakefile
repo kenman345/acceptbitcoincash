c68720cc2da7dc6b57c60d641b7a53c9ef764938
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-01-31 21:19:38 +0000

    Deploy kenman345/acceptbitcoincash to github.com/kenman345/acceptbitcoincash.git:gh-pages

--- Rakefile
+++ Rakefile
@@ -18,7 +18,10 @@ task proof: 'build' do
     './_site', \
     assume_extension: true, \
     check_html: true, \
-    disable_external: true
+    disable_external: true, \
+	log_level: 'debug', \
+	url_ignore: ['/add'], \
+	verbose: true
   ).run
 end
 
@@ -27,13 +30,36 @@ task proof_external: 'build' do
     './_site', \
     assume_extension: true, \
     check_html: true, \
+	external_only: false, \
+	verbose: true, \
+	log_level: 'info', \
+	url_ignore: ['/add'], \
+	http_status_ignore: [0, 301, 302, 403, 503], \
     cache: { timeframe: '1w' }, \
     hydra: { max_concurrency: 12 }
   ).run
 end
 
+namespace :docker do
+  desc "build docker images"
+  task :build do
+    puts "Generating stats (HTML partial) of websites supporting Bitcoin Cash"
+    Dir.chdir(File.join('.', 'scripts', 'python')) do
+      puts `python ./bchAccepted.py`
+    end
+    puts "Generating static files for nginx"
+    puts `bundle exec jekyll build`
+    puts "Building acceptbitcoincash docker image"
+    puts `docker build -t kenman345/acceptbitcoincashdocker .`
+  end
+end
+
 task :verify do
   ruby './verify.rb'
+end
+
+task :verify_images do
+  ruby './verify_images.rb'
 end
 
 RuboCop::RakeTask.new
--- js/app.js
+++ js/app.js
@@ -1,9 +1,133 @@
 // When DOM elements are ready, excluding images
 $(document).ready(function () {
+
+  // Get all URL parameters
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+    }
+  };
+
+  // Check if URL parameter exists to filter by BCH-only
+  if (getUrlParameter('filter') == 'all') {
+    $('#show-bch-only').prop('checked', false);
+  }
+
+  // Clear the BCH-only view
+  $('.clear-bch-only').click(function () {
+    $('#show-bch-only').prop('checked', false);
+    BCHfilter();
+  });
+
   // Check if URL references specific category
   if (window.location.hash && window.location.hash.indexOf('#') > -1) {
     openCategory(window.location.hash.substring(1));
   }
+
+  // Some frilly animations on click of the main Bitcoin Cash logo
+  $('#coin-toggle').click(function (){
+    coinEffect();
+  });
+
+  // Stick the BCH-only filter to the top on scroll
+  $(".bch-only").fixTo('html', {
+    useNativeSticky: false
+  });
+
+  // Scroll to the top via floating action button and filter bar link, then pop some flair
+  $('.fab button:nth-child(1), #top-btn-top').on('click', function () {
+    var body = $("html, body");
+    body.stop().animate({scrollTop:0}, 500, 'swing', function () {
+      coinEffect();
+
+      // Restores the opened category hash in URL, but causes Firefox to skip back to it
+      //if (window.location.hash && window.location.hash.indexOf('#') > -1) {
+        //document.location.hash = window.location.hash.substring(1);
+      //}
+    });
+  });
+
+  // Scroll to the main content grid
+  $('#skip-to-content').on('click', function () {
+    var body = $("html, body");
+    body.stop().animate({scrollTop: $('#maingrid').offset().top - 64}, 500, 'swing');
+  });
+
+  // Scroll to the search field and focus it via floating action button and filter bar link
+  $('.fab button:nth-child(2), #top-btn-search').on('click', function () {
+    var body = $("html, body");
+    body.stop().animate({scrollTop: $('#search-wrapper').offset().top}, 500, 'swing');
+    $('#search-wrapper input').focus();
+  });
+
+  // Clear and collapse all open categories
+  $('.fab button:nth-child(3)').on('click', function () {
+    if (isSearching) jets.options.didSearch( $('#bch-merchant-search').val() );
+    if (isSearching == false) {
+      $('.website-table').slideUp();
+      var body = $("html, body");
+      body.stop().animate({scrollTop: $('.category h5 i.active-icon').offset().top - 120}, 1000, 'swing');
+      $('.category h5 i').removeClass('active-icon');
+      $(this).css('display', 'none');
+      document.location.hash = '';
+    } else {
+      if ($(this).hasClass('attention')) {
+        $(this).removeClass('attention');
+        $("#bch-merchant-search").removeClass('attention');
+      } else {
+        $(this).addClass('attention');
+        $("#bch-merchant-search").addClass('attention');
+      }
+    }
+  });
+
+  // Clear the active search terms
+  $('button#search-clear').on('click', function () {
+    $('#search-wrapper input#bch-merchant-search').val('');
+    $('#no-results').css('display', 'none');
+    $('.category').show();
+    $('.website-table').hide();
+    $('#maingrid').css('visibility', 'visible');
+    $('#search-wrapper input#bch-merchant-search').focus();
+    $('head style').html("");
+  });
+
+  $('.submit-new').on('click', function () {
+    $('.ui.long.modal.google-form').modal('show');
+    return false;
+  });
+
+  $('#ama-merchant').on('click', function () {
+    $('.ui.modal.ama-merchant').modal('show');
+  });
+
+  $('#ama-customer').on('click', function () {
+    $('.ui.modal.ama-customer').modal('show');
+  });
+
+  $('#assets').on('click', function () {
+    $('.ui.modal.assets').modal('show');
+  });
+
+  $('#art-collections').on('click', function () {
+    $('.ui.modal.art-collections').modal('show');
+  });
+
+  $('#abci-logo, h1 i, #about-this-site').on('click', function () {
+    $('.ui.modal.about-this-site').modal('show');
+  });
+
+  $('#show-disclaimer').on('click', function () {
+    $('.ui.modal.disclaimer').modal('show');
+  });
 
   // Unveil images 50px before they appear
   $('img').unveil(50);
@@ -13,7 +137,44 @@ $(document).ready(function () {
     hoverable: true
   });
   $('a.popup.exception').popup();
+
+  // Display progress counter for sites accepting BCH out of total sites listed
+  $('.ui.bch-progress').progress({
+    label: 'percent',
+    showActivity: false
+  });
+
+  // Retrieve latest production version
+  //getLatestRelease();
 });
+
+/**
+ * Draw a neat animation on the main Bitcoin Cash logo at the top of the page
+ */
+function coinEffect() {
+  var mainCoin = $('#main-coin path.glyph');
+  var leftSideCoin = $('.top-side-left-side, .top-side-left-side-force');
+  var rightSideCoin = $('.top-side-right-side, .top-side-right-side-force');
+
+  mainCoin.removeClass("anim-glyph anim-glyph-force");
+  leftSideCoin.removeClass("top-side-left-side top-side-left-side-force");
+  rightSideCoin.removeClass("top-side-right-side top-side-right-side-force");
+  setTimeout(
+    function(){ mainCoin.addClass('anim-glyph-force') }
+  , 1);
+  setTimeout(
+    function(){ leftSideCoin.addClass('top-side-left-side-force') }
+  , 1);
+  setTimeout(
+    function(){ rightSideCoin.addClass('top-side-right-side-force') }
+  , 1);
+}
+
+function getLatestRelease() {
+  var githubfeed = $.getJSON("https://api.github.com/repos/acceptbitcoincash/acceptbitcoincash/releases/latest", function(data){
+    $("span.version").html('Current Release: <a href="https://github.com/acceptbitcoincash/acceptbitcoincash/releases/latest" target="_blank" data-tooltip="Read the ' + data.tag_name + ' release notes" data-position="top center" data-inverted=""><i class="tag icon"></i><b>' + data.tag_name + '</b></a> &nbsp;&bull;&nbsp; <a href="https://github.com/acceptbitcoincash/acceptbitcoincash/compare/' + data.tag_name + '...master" target="_blank" data-tooltip="View a list of approved commits that have not yet been deployed to this site" data-position="top center" data-inverted="">Upcoming changes <i class="sign in icon"></i></a>');
+  });
+}
 
 /**
  * Create an event that is called 500ms after the browser
@@ -30,15 +191,16 @@ $(window).resize(function () {
 
 var isSearching = false;
 var jets = new Jets({
-  searchTag: '#jets-search',
-  contentTag: '.jets-content',
+  searchTag: '#bch-merchant-search',
+  contentTag: '.bch-merchant-content',
   didSearch: function (searchPhrase) {
     document.location.hash = '';
     $('#no-results').css('display', 'none');
+    $('#maingrid').css('visibility', 'visible');
     $('.category h5 i').removeClass('active-icon');
     // Two separate table layouts are used for desktop/mobile
     var platform = ($(window).width() > 768) ? 'desktop' : 'mobile';
-    var content = $('.' + platform + '-table .jets-content');
+    var content = $('.' + platform + '-table .bch-merchant-content');
     var table = $('.' + platform + '-table');
 
     // Non-strict comparison operator is used to allow for null
@@ -46,6 +208,7 @@ var jets = new Jets({
       // Show all categories when no search term is entered
       $('.website-table').css('display', 'none');
       $('.website-table .label').css('display', 'none');
+      $('#search-clear').css('display', 'none');
       $('.category').show();
       $('table').show();
       isSearching = false;
@@ -66,11 +229,18 @@ var jets = new Jets({
 
       if (table.children().length == table.children(':hidden').length) {
           $('#no-results').css('display', 'block');
+          $('#maingrid').css('visibility', 'hidden');
       }
 
+      $('#search-clear').fadeIn('slow');
+
       isSearching = true;
+
+      //$('html, body').stop().animate({scrollTop: $('#maingrid').offset().top - 120}, 500, 'swing');
+      $('html, body').scrollTop($('#maingrid').offset().top - 64);
     }
   },
+  addImportant: true,
   // Process searchable elements manually
   manualContentHandling: function(tag) {
     return $(tag).find('.title > a.name').text();
@@ -82,7 +252,7 @@ var jets = new Jets({
  * after re-sizing the screen and close all categories after re-sizing
  */
 $(window).on('resizeEnd', function () {
-  if (isSearching) jets.options.didSearch($('#jets-search').val());
+  if (isSearching) jets.options.didSearch( $('#bch-merchant-search').val() );
 });
 
 // Display tables and color category selectors
@@ -90,6 +260,33 @@ $('.category').click(function () {
   var name = $(this).attr('id');
   isOpen(name) ? closeCategory(name) : openCategory(name);
 });
+
+/**
+ * Toggle visibility of merchants who accept Bitcoin Cash
+ */
+$('.z-switch').click(function () {
+  BCHfilter();
+});
+
+/**
+ * Check if the user wants to filter by Bitcoin Cash only
+ */
+function BCHfilter() {
+ if ($('#show-bch-only').is(':checked')) {
+    $('.no-bch').css('display', 'none');
+    $('.bch-only-none-found').css('display', 'table-row');
+    $('.bch-only-none-found-mobile').css('display', 'block');
+    $('.bch-only-hidden').css('opacity', '0.4');
+    if (isSearching) jets.options.didSearch( $('#bch-merchant-search').val() );
+  } else {
+    $('.website-table:visible img').trigger('unveil');
+    $('.bch-only-none-found, .bch-only-none-found-mobile').css('display', 'none');
+    $('.bch-only-hidden').css('opacity', '1');
+    $('.mobile-table .no-bch').css('display', 'block');
+    $('.desktop-table .no-bch').css('display', 'table-row');
+    if (isSearching) jets.options.didSearch( $('#bch-merchant-search').val() );
+  }
+}
 
 /**
  * Checks if a category is open
@@ -110,6 +307,8 @@ function openCategory(category) {
   // Close all active categories
   $('.category h5 i').removeClass('active-icon');
   $('.website-table').css('display', 'none');
+  $('.fab button:nth-child(3)').css('display', 'inline-block');
+  BCHfilter();
 
   // Place the category being viewed in the URL bar
   window.location.hash = category;
@@ -117,15 +316,20 @@ function openCategory(category) {
   var icon = $('#' + category + ' h5 i');
   icon.addClass('active-icon');
   if ($(window).width() > 768) {
-    $('#' + category + '-desktoptable').css('display', 'block');
+    $('#' + category + '-desktoptable').slideDown('slow');
+
+    // Scroll smoothly to category selector
+    var body = $("html, body");
+    body.stop().animate({scrollTop: icon.offset().top - 120}, 1000, 'swing');
+
   } else {
-    $('#' + category + '-mobiletable').css('display', 'block');
+    $('#' + category + '-mobiletable').css('display','block');
+    // Quickly snap to category selector
+    var body = $("html, body");
+    body.stop().animate({scrollTop: icon.offset().top - 120}, 1000, 'swing');
+    //document.location.hash = category;
   }
 
-  // Scroll smoothly to category selector
-  $('html, body').animate({
-    scrollTop: icon.offset().top - 25
-  }, 1000);
 }
 
 /**
@@ -134,7 +338,8 @@ function openCategory(category) {
  * @param category The id of a category as a string
  */
 function closeCategory(category) {
+  $('.' + category + '-table').slideUp();
   $('#' + category + ' h5 i').removeClass('active-icon');
-  $('.' + category + '-table').css('display', 'none');
-  document.location.hash = '';
+  history.pushState('', document.title, window.location.pathname);
+  $('.fab button:nth-child(3)').css('display', 'none');
 }
