0125b670ca0eb6af73e2ec5913c5c73eeac1e5d0
Ken Salomon <Kensalomon@gmail.com>
2018-05-05 10:25:50 -0400

    fixing things up

--- assets/js/base.js
+++ assets/js/base.js
@@ -3,9 +3,7 @@
 {% capture page_js %}
 {% include js/jquery.unveil.min.js %}
 {% include js/ticker.js %}
-</script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fixto/0.5.0/fixto.min.js" integrity="sha256-ZQGLNISOtnQdRdKwA8Ou7EyEVtnE2k1oBZPBr5NcGQs=" crossorigin="anonymous"></script>
-<script>
+{% include js/fixto.min.js %}
 {% include js/app.js listing="false" search="false" %}
 {% endcapture %}
 
