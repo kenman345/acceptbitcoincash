7ea11651894f1ea6025795715da2134401fc739d
Ken Salomon <Kensalomon@gmail.com>
2018-05-05 10:09:15 -0400

    removing some build parts

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,9 +1,7 @@
 ---
 ---
 {% capture page_js %}
-{% include js/jquery.unveil.min.js %}
 {% include js/ticker.js %}
-{% include js/fixto.min.js %}
 {% include js/app.js listing="false" search="false" %}
 {% endcapture %}
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,7 @@
 
 ---
 {% capture page_js %}
-{% include js/jquery.unveil.min.js %}
 {% include js/ticker.js %}
-{% include js/fixto.min.js %}
 {% include js/app.js listing="true" search="true" %}
 {% endcapture %}
 
