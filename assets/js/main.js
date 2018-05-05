68c2562ce0ff321aa483fd4dfea7b0f78428d0de
Ken Salomon <Kensalomon@gmail.com>
2018-05-05 10:22:41 -0400

    trying this

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@
 {% capture page_js %}
 {% include js/jquery.unveil.min.js %}
 {% include js/ticker.js %}
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fixto/0.5.0/fixto.min.js" integrity="sha256-ZQGLNISOtnQdRdKwA8Ou7EyEVtnE2k1oBZPBr5NcGQs=" crossorigin="anonymous"></script>
+<script>
 {% include js/app.js listing="true" search="true" %}
 {% endcapture %}
 
